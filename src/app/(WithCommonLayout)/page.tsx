226590fbc4e799406962e031d39007517a448a33
rehanulhb <rehanulh@gmail.com>
2025-11-23 18:21:19 +0100

    Setting and Retrieving Logged-In user Info from Cookies

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,11 +1,13 @@
+import { getCurrentUser } from "@/services/AuthService";
 
-
-const HomePage = () => {
-    return (
-        <div>
-            <h1>Welcome to Ecommerce Home Page</h1>
-        </div>
-    );
+const HomePage = async () => {
+  const user = await getCurrentUser();
+  console.log(user);
+  return (
+    <div>
+      <h1>Welcome to Ecommerce Home Page</h1>
+    </div>
+  );
 };
 
 export default HomePage;
