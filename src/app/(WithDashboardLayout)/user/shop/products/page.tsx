757f9ad678ce75e7046c063cbf1dd15a7a2ffeb6
rehanulhb <rehanulh@gmail.com>
2025-12-06 22:55:27 +0100

    Integrate Pagination with Table

--- src/app/(WithDashboardLayout)/user/shop/products/page.tsx
+++ src/app/(WithDashboardLayout)/user/shop/products/page.tsx
@@ -1,8 +1,13 @@
 import ManageProducts from "@/components/modules/shop/product";
 import { getAllProducts } from "@/services/Product";
 
-const ManageProductsPage = async () => {
-  const { data, meta } = await getAllProducts();
+const ManageProductsPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ page: string }>;
+}) => {
+  const { page } = await searchParams;
+  const { data, meta } = await getAllProducts(page, "3");
   return (
     <div>
       <ManageProducts products={data} meta={meta} />
