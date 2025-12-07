0b9cd73493b4c726931567608e1874995c392642
rehanulhb <rehanulh@gmail.com>
2025-12-07 23:42:58 +0100

    Connecting Redux with the App

--- src/components/modules/cart/PaymentDetails.tsx
+++ src/components/modules/cart/PaymentDetails.tsx
@@ -75,20 +75,20 @@ export default function PaymentDetails() {
       <div className="space-y-2 mt-4">
         <div className="flex justify-between">
           <p className="text-gray-500 ">Subtotal</p>
-          <p className="font-semibold">{currencyFormatter(subTotal)}</p>
+          {/* <p className="font-semibold">{currencyFormatter(subTotal)}</p> */}
         </div>
         <div className="flex justify-between">
           <p className="text-gray-500 ">Discount</p>
-          <p className="font-semibold">{currencyFormatter(0)}</p>
+          {/* <p className="font-semibold">{currencyFormatter(0)}</p> */}
         </div>
         <div className="flex justify-between">
           <p className="text-gray-500 ">Shipment Cost</p>
-          <p className="font-semibold">{currencyFormatter(shippingCost)}</p>
+          {/* <p className="font-semibold">{currencyFormatter(shippingCost)}</p> */}
         </div>
       </div>
       <div className="flex justify-between mt-10 mb-5">
         <p className="text-gray-500 ">Grand Total</p>
-        <p className="font-semibold">{currencyFormatter(grandTotal)}</p>
+        {/* <p className="font-semibold">{currencyFormatter(grandTotal)}</p> */}
       </div>
       <Button
         onClick={handleOrder}
