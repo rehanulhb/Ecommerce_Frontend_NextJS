0116d5248f0d5a10cb7c8c8bf11413bbaca3ff27
rehanulhb <rehanulh@gmail.com>
2025-12-12 21:49:42 +0100

    Setting Up Redux Persist

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -3,15 +3,23 @@
 import { AppStore, makeStore } from "@/redux/store";
 import { ReactNode, useRef } from "react";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
-const StoreProvider = ({ children }: { children: ReactNode }) => {
+export default function StoreProvider({ children }: { children: ReactNode }) {
   const storeRef = useRef<AppStore>(undefined);
 
   if (!storeRef.current) {
     storeRef.current = makeStore();
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
-};
+  const persistedStore = persistStore(storeRef.current);
 
-export default StoreProvider;
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={"Loading..."} persistor={persistedStore}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
+}
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./features/cartSlice";
+import cartReducer from "./features/cartSlice";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "./storage";
 
 //! We will not do this
 //! This is a global variable so we will avoid this
 // const store = configureStore({});
 
+const persistOptions = {
+  key: "cart",
+  storage,
+};
+
+const persistedCart = persistReducer(persistOptions, cartReducer);
+
 export const makeStore = () => {
   return configureStore({
     reducer: {
-      cart: cartSlice,
+      cart: persistedCart,
     },
+    middleware: (getDefaultMiddlewares) =>
+      getDefaultMiddlewares({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
   });
 };
 
